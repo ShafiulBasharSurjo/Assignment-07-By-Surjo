e4cf63c7420eb31b84a0e54b554ecb733d2f45e5
ShafiulBasharSurjo <shafiulbasharprodhan@gmail.com>
2026-04-14 12:43:38 +0600

    Cards are done

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -4,20 +4,22 @@ import { FaPlus } from "react-icons/fa";
 const Banner = () => {
   return (
     <>
-      <div className=" bg-base-200 max-w-277.5 container mx-auto">
-        <div className="hero-content text-center ">
-          <div className="max-w-md">
-            <h1 className="text-3xl font-bold">
-              Friends to keep close in your life
-            </h1>
-            <p className="text-gray-700 py-6 text-small">
-              Your personal shelf of meaningful connections. Browse, tend, and
-              nurture the relationships that matter most.
-            </p>
-            <button className="btn bg-[#244D3F] text-white">
-              <FaPlus />
-              Add a Friend
-            </button>
+      <div className="bg-base-200">
+        <div className="max-w-277.5 container mx-auto">
+          <div className="hero-content text-center ">
+            <div className="max-w-md">
+              <h1 className="text-3xl font-bold">
+                Friends to keep close in your life
+              </h1>
+              <p className="text-gray-700 py-6 text-small">
+                Your personal shelf of meaningful connections. Browse, tend, and
+                nurture the relationships that matter most.
+              </p>
+              <button className="btn bg-[#244D3F] text-white">
+                <FaPlus />
+                Add a Friend
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/homepage/FriendsData.jsx
+++ src/components/homepage/FriendsData.jsx
@@ -6,49 +6,62 @@ const FriendsData = () => {
   const friends = use(friendsPromise);
 
   const statusStyles = {
-    "Almost Due": "bg-[#E9A23B] text-white",
-    Overdue: "bg-[#D9534F] text-white",
-    "On-Track": "bg-[#1D4733] text-white",
+    "On-Track": "badge badge-success",
+    Overdue: "badge badge-error",
+    "Almost Due": "badge badge-warning",
   };
   return (
     <>
-      <div className=" bg-base-200 max-w-277.5 container mx-auto">
-        <h2 className="font-bold text-3xl">Your Friends</h2>
+      <div className=" bg-base-200 py-10 ">
+        <div className="max-w-277.5 container mx-auto px-4">
+          <h2 className="font-bold text-3xl">Your Friends</h2>
 
-        <div>
-          {friends.map((friend) => {
-            return (
-              <div key={friend.id} className="card bg-base-100 w-96 shadow-sm">
-                <figure className="mb-4">
-                  <img
-                    src={friend.picture}
-                    alt={friend.name}
-                    className="w-20 h-20 rounded-full object-cover shadow-sm"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="text-[#1A2631] text-lg font-black tracking-tight mb-4">
-                    {friend.name}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-10 max-w-277.5 mx-auto px-4 mt-6">
+            {friends.map((friend) => {
+              return (
+                <div
+                  key={friend.id}
+                  className="card bg-base-100 w-full shadow-sm border border-gray-100"
+                >
+                  <figure className="mb-4 mt-6">
+                    <img
+                      src={friend.picture}
+                      alt={friend.name}
+                      className="w-20 h-20 border-2 border-white rounded-full object-cover shadow-sm"
+                    />
+                  </figure>
+                  <div className="flex flex-col items-center">
+                    <h2 className="text-[#1A2631] text-lg font-black tracking-tight mb-4">
+                      {friend.name}
+                    </h2>
                     <p className="text-[#5D6B82] text-xs font-medium mt-1 mb-4">
                       {friend.days_since_contact}D ago
                     </p>
-                  </h2>
-                  <div className="card-actions justify-center flex-col items-center">
-                    <div className="mb-4">
-                      <span className="bg-[#D1FAE5] text-[#065F46] px-3 py-1 rounded-full text-[10px] font-black uppercase tracking-widest">
-                        {friend.category || "General"}
-                      </span>
-                    </div>
-                    <div
-                      className={`px-5 py-2 rounded-full text-xs font-bold shadow-sm ${statusStyles[friend.status]}`}
-                    >
-                      {friend.status}
+                    <div className="card-actions justify-center flex-col items-center">
+                      <div className="flex flex-col items-center">
+                        <div className="flex flex-wrap justify-center gap-2 mb-4">
+                          {friend.tags.map((tag, index) => (
+                            <span
+                              key={index}
+                              className="bg-[#D1FAE5] text-[#065F46] px-3 py-1 rounded-full text-[10px] font-black uppercase tracking-widest"
+                            >
+                              {tag}
+                            </span>
+                          ))}
+                        </div>
+                      </div>
+
+                      <div
+                        className={`mb-4 px-6 py-2 rounded-full text-xs font-bold shadow-sm ${statusStyles[friend.status]}`}
+                      >
+                        {friend.status}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     </>
